55140a442b203c4b73f0164ead08355a13893b8c
Ashker4477 <muhammedashker3@gmail.com>
2022-04-17 17:50:12 +0530

    server modified

--- back-end/server.js
+++ back-end/server.js
@@ -28,13 +28,13 @@ app.get('/api/config/paypal', (req, res) => {
 });
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.get('/', (req, res) => {
-    res.send('<h1>Server is Ready one one</h1>');
-});
-// app.use(express.static(path.join(__dirname, 'front-end/build')));
-// app.get('*', (req,res)=>{
-//     res.sendFile(path.join(__dirname,'front-end/build/index.html'))
-// })
+// app.get('/', (req, res) => {
+//     res.send('<h1>Server is Ready one one</h1>');
+// });
+app.use(express.static(path.join(__dirname, 'front-end/build')));
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname,'front-end/build/index.html'))
+})
 // async-handler middleware
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message })
