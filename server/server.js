c21b4dafc0d22adae4eadeec817de72ae72a89fa
Muhammed Ashker <ashker@faircodetech.com>
2022-12-02 22:32:41 +0530

    server modified.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
-import path from 'path';
+import dotenv from "dotenv";
+import path from "path";
 // import data from './data.js';
 import productRouter from "./Router/ProductRouter.js";
 import userRouter from "./Router/userRouter.js";
@@ -15,30 +15,33 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL).then(() =>
-    console.log('DB is Ready')
-).catch((err) => console.error(err));
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("DB is Ready"))
+  .catch((err) => console.error(err));
 
-app.use('/api/uploads', uploadRouter);
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
-app.use('/api/orders', orderRouter);
-app.get('/api/config/paypal', (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+app.use("/api/uploads", uploadRouter);
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 // app.get('/', (req, res) => {
 //     res.send('<h1>Server is Ready one one</h1>');
 // });
-app.use(express.static(path.join(__dirname, 'front-end/build')));
-app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname,'front-end/build/index.html'))
-})
+app.use(express.static(path.join(__dirname, "..", "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client/build/index.html"));
+});
 // async-handler middleware
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message })
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT;
-app.listen(port, () => console.log(`Server is ready at https://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server is ready at https://localhost:${port}`)
+);
